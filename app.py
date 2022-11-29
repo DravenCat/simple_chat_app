426f1c91f77e76132fc83048e6edd2db2c96f668
Dezhi Ren <dezhi.ren@mail.utoronto.ca>
2022-11-28 19:54:57 -0500

    rebuild

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import config
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
+    app.config.from_object(config)
     app.run()
